ac3a402709b82edbd38c53c062d9d57aadc21bf9
daruvurisai <daruvurisai@gmail.com>
2021-06-07 15:28:55 -0500

    SumOddRange Java Program

---
+++ src/SumOdd.java
@@ -0,0 +1,39 @@
+public class SumOdd {
+    public static boolean isOdd(int number) {
+        if ((number > 0) && (number % 2 != 0)) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (start > 0 && end > 0 && end >= start) {
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        } else {
+            return -1;
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args){
+        isOdd(7);
+        isOdd(9);
+        System.out.println(isOdd(7));
+        sumOdd(100,100);
+        System.out.println(sumOdd(100,100));
+        System.out.println(sumOdd(10,5));
+    }
+
+}
